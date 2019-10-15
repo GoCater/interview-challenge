692ef1c69056ac8ca648949f6ee4ba0e30c4a436
Thomas Florelli <perso.thomas.florelli@gmail.com>
2019-10-15 13:46:20 +0200

    Clarified react app

--- frontend/react-tweets/src/App.js
+++ frontend/react-tweets/src/App.js
@@ -4,26 +4,30 @@ import Nav from 'react-bootstrap/Nav';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const setActiveTab = () => {};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-const App = () => {
-  return (
-    <div className="app">
-      <Nav variant="tabs" activeKey="tweets">
-        <Nav.Item>
-          <Nav.Link eventKey="tweets" onSelect={() => setActiveTab('tweets')}>
-            Tweets
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="create-tweet" onSelect={() => setActiveTab('createTweet')}>
-            Create Tweet
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-      <div className="tab-content"></div>
-    </div>
-  );
+  render() {
+    return (
+      <div className="app">
+        <Nav variant="tabs" activeKey="tweets">
+          <Nav.Item>
+            <Nav.Link eventKey="tweets" onSelect={() => this.setActiveTab('tweets')}>
+              Tweets
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="create-tweet" onSelect={() => this.setActiveTab('createTweet')}>
+              Create Tweet
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <div className="tab-content"></div>
+      </div>
+    );
+  }
 };
 
 export default App;
