9e4130130cfb7441d0a171be95a6a864b38e87e7
Thomas Florelli <perso.thomas.florelli@gmail.com>
2019-10-15 14:42:17 +0200

    Fixed typo on health route

--- frontend/react-tweets/src/utils/remote-actions.js
+++ frontend/react-tweets/src/utils/remote-actions.js
@@ -5,5 +5,5 @@ const API_URL = 'http://localhost:4000'
 const getUrl = endpoint => `${API_URL}/api${endpoint}`;
 
 export const healthCheck = () => {
-  return axios.get(getUrl('/heath'));
+  return axios.get(getUrl('/health'));
 };
